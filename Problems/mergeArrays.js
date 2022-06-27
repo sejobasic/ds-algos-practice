d085e0a012cf1fc3f792d2612d9b3bf32a0b8fb2
Sejo Basic <sejoob@gmail.com>
2022-06-26 20:34:29 -0700

    Merge Arrays Function

--- Problems/mergeArrays.js
+++ Problems/mergeArrays.js
@@ -1,4 +1,5 @@
-// Create a function that merges two sorted arrays
+// Write a function that merges two sorted arrays
+// Write a function that sorts the given array
 
 // Create an empty array, take a  look at the smallest values in each input array
 // Create a starting point for each array
@@ -35,3 +36,17 @@ function mergeArrays(arr1, arr2) {
 }
 
 mergeArrays([1, 2, 6, 9], [34, 60, 73])
+
+// Break up the array into halves until you have arrays that are empty or have one element
+// Once you have smaller sorted arrays, merge those arrays with 
+// other sorted arrays until your back at the full length of the array
+// Once the array has been merged back together
+// Return the merged and sorted array
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr
+  let mid = Math.floor(arr.length/2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return mergeArrays(left, right)
+}
